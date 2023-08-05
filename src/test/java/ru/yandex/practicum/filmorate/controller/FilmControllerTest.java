00bc465d66ca5ec1cd9f285da2a84e9b310ffcc3
Bogdan <ggbogdanov-03@mail.ru>
2023-08-05 18:46:48 +1000

    ТЗ-9 Add: friends and likes. #4

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,87 +1,80 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.time.LocalDate;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = FilmController.class)
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class FilmControllerTest {
-
-
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
     private ObjectMapper objectMapper;
 
-    private Film testFilm;
-
-    @BeforeEach
-    protected void init() {
-        testFilm = Film.builder()
-                .name("Фильм One")
-                .description("Описание фильма")
-                .releaseDate(LocalDate.of(1999, 04, 07))
-                .duration(99)
-                .build();
-
-    }
-
 
     @Test
-    void createNewCorrectFilm_isOkTest() throws Exception {
-        mockMvc.perform(post("/films")
-                        .content(objectMapper.writeValueAsString(testFilm))
-                        .contentType("application/json"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.id").value("1"));
+    void shouldNotAddFilmWithEmptyName() throws Exception {
+        mockMvc.perform(
+                post("/films")
+                        .content("{\n" +
+                                "  \"name\": \"\",\n" +
+                                "  \"description\": \"adipisicing\",\n" +
+                                "  \"releaseDate\": \"1967-03-25\",\n" +
+                                "  \"duration\": 100\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
     }
 
     @Test
-    void createFilm_NameIsBlank_badRequestTest() throws Exception {
-        testFilm.setName("");
-        mockMvc.perform(post("/films")
-                        .content(objectMapper.writeValueAsString(testFilm))
-                        .contentType("application/json"))
-                .andExpect(status().isBadRequest());
+    void shouldNotAddFilmWithTooLongDescription() throws Exception {
+        mockMvc.perform(
+                post("/films")
+                        .content("{\n" +
+                                "  \"name\": \"nisi eiusmod\",\n" +
+                                "  \"description\": \"qqqqqqqqqqwwwwwwwwwweeeeeeeeeerrrrrrrrrrttttttttttyyyyyyyyyy" +
+                                "qqqqqqqqqqqwwwwwwwwwweeeeeeeeeerrrrrrrrrrttttttttttyyyyyyyyyyqqqqqqqqqqwwwwwwwwww" +
+                                "wwwwwwwwwweeeeeeeeeerrrrrrrrrrttttttttttyyyyyyyyyyqqqqqqqqqqqqqqqqqqqqqqqqqq\",\n" +
+                                "  \"releaseDate\": \"1967-03-25\",\n" +
+                                "  \"duration\": 100\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
     }
 
     @Test
-    void createFilm_IncorrectDescription_badRequestTest() throws Exception {
-        testFilm.setDescription("Ошибка 405 (Method Not Allowed) возникает, когда сервер не может обработать HTTP-запрос" +
-                " из-за неподдерживаемого метода, который был использован в запросе, что может быть вызвано некорректной " +
-                "настройкой сервера или неправильным использованием HTTP-методов приложением.");
-        mockMvc.perform(post("/films")
-                        .content(objectMapper.writeValueAsString(testFilm))
-                        .contentType("application/json"))
-                .andExpect(status().isBadRequest());
+    void shouldNotAddFilmWithTooOldDate() throws Exception {
+        mockMvc.perform(
+                post("/films")
+                        .content("{\n" +
+                                "  \"name\": \"fdgfdg fdgdfgffdfgg\",\n" +
+                                "  \"description\": \"dfgdfgfdgdf\",\n" +
+                                "  \"releaseDate\": \"1400-03-25\",\n" +
+                                "  \"duration\": 100\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
     }
 
     @Test
-    void createFilm_RealiseDateInFuture_badRequestTest() throws Exception {
-        testFilm.setReleaseDate(LocalDate.of(2024, 01, 01));
-        mockMvc.perform(post("/films")
-                        .content(objectMapper.writeValueAsString(testFilm))
-                        .contentType("application/json"))
-                .andExpect(status().isBadRequest());
+    void shouldNotAddFilmWithNegativeDuration() throws Exception {
+        mockMvc.perform(
+                post("/films")
+                        .content("{\n" +
+                                "  \"name\": \"fgdfgfdgdf dfgdfgd\",\n" +
+                                "  \"description\": \"afdgdgfdgfdng\",\n" +
+                                "  \"releaseDate\": \"1967-03-25\",\n" +
+                                "  \"duration\": -100\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
     }
-
-    @Test
-    void createFilm_RealiseDateBeforeFirstFilmDate_badRequestTest() throws Exception {
-        testFilm.setReleaseDate(LocalDate.of(1800, 10, 10));
-        mockMvc.perform(post("/films")
-                        .content(objectMapper.writeValueAsString(testFilm))
-                        .contentType("application/json"))
-                .andExpect(status().isBadRequest());
-    }
-
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,87 +1,84 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
-
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
-
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
 
-    private User testUser;
 
-    @BeforeEach
-    protected void init() {
-        testUser = User.builder()
-                .email("mail@mail.ru")
-                .login("NameName")
-                .name("Nick Name")
-                .birthday(LocalDate.of(1987, 4, 14))
-                .build();
-
+    @Test
+    void shouldNotAddUserWhenWrongEmailFormat() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .content("{\n" +
+                                "  \"login\": \"alone\",\n" +
+                                "  \"name\": \"est a121212cing\",\n" +
+                                "  \"email\": \"mailmail.ru\",\n" +
+                                "  \"birthday\": \"1946-08-20\"\n" +
+                                "}" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
     }
 
 
     @Test
-    void createNewCorrectUser_Test() throws Exception {
-        mockMvc.perform(post("/users")
-                        .content(objectMapper.writeValueAsString(testUser))
-                        .contentType("application/json"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.id").value("1"));
+    void shouldNotAddUserWhenLoginIsEmpty() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .content("{\n" +
+                                "  \"login\": \"\",\n" +
+                                "  \"name\": \"est a324324icing\",\n" +
+                                "  \"email\": \"mail@mail.ru\",\n" +
+                                "  \"birthday\": \"1946-08-20\"\n" +
+                                "}" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
     }
 
     @Test
-    void createUser_NameIsBlank_NameIsLoginTest() throws Exception {
-        testUser.setName("");
-        mockMvc.perform(post("/users")
-                        .content(objectMapper.writeValueAsString(testUser))
-                        .contentType("application/json"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.name").value(""));
+    void shouldAddUserWhenNameIsEmpty() throws Exception {
+        mockMvc.perform(
+                        post("/users")
+                                .content("{\n" +
+                                        "  \"login\": \"nfdofdof\",\n" +
+                                        "  \"name\": \"\",\n" +
+                                        "  \"email\": \"mail@mail.ru\",\n" +
+                                        "  \"birthday\": \"1946-08-20\"\n" +
+                                        "}" +
+                                        "}")
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"name\":\"dsdfsdfsd\"")));
     }
 
     @Test
-    void createUser_IncorrectEmailTest() throws Exception {
-        testUser.setEmail("incorrectEmail.ru");
-        mockMvc.perform(post("/users")
-                        .content(objectMapper.writeValueAsString(testUser))
-                        .contentType("application/json"))
-                .andExpect(status().isBadRequest());
+    void shouldNotAddUserWhenBirthdayIsInTheFuture() throws Exception {
+        mockMvc.perform(
+                post("/users")
+                        .content("{\n" +
+                                "  \"login\": \"sdfsdf\",\n" +
+                                "  \"name\": \"est adipisdsfs342icing\",\n" +
+                                "  \"email\": \"mail@mail.ru\",\n" +
+                                "  \"birthday\": \"2946-08-20\"\n" +
+                                "}" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
     }
-
-    @Test
-    void createUser_LoginIsBlank_badRequestTest() throws Exception {
-        testUser.setLogin("");
-        mockMvc.perform(post("/users")
-                        .content(objectMapper.writeValueAsString(testUser))
-                        .contentType("application/json"))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void createUser_BirthdayInFuture_badRequestTest() throws Exception {
-        testUser.setBirthday(LocalDate.parse("2024-10-12"));
-        mockMvc.perform(post("/users")
-                        .content(objectMapper.writeValueAsString(testUser))
-                        .contentType("application/json"))
-                .andExpect(status().isBadRequest());
-    }
-
 }
