218247229dea6ea523b4487a9138521dfa3ffd7f
Bogdan <ggbogdanov-03@mail.ru>
2023-09-04 16:29:36 +1000

    ТЗ-11 Add: database. #1.4

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,55 +1,7 @@
-INSERT INTO PUBLIC.STATUSES
-(STATUS_NAME)
-VALUES('REQUEST');
 
-INSERT INTO PUBLIC.STATUSES
-(STATUS_NAME)
-VALUES('ACTIVE');
 
-INSERT INTO PUBLIC.STATUSES
-(STATUS_NAME)
-VALUES('DELETED');
+INSERT INTO genres (genre_name)
+VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
 
-INSERT INTO PUBLIC.MPA
-(name)
-VALUES('G');
-
-INSERT INTO PUBLIC.MPA
-(name)
-VALUES('PG');
-
-INSERT INTO PUBLIC.MPA
-(name)
-VALUES('PG-13');
-
-INSERT INTO PUBLIC.MPA
-(name)
-VALUES('R');
-
-INSERT INTO PUBLIC.MPA
-(name)
-VALUES('NC-17');
-
-INSERT INTO PUBLIC.GENRES
-(name)
-VALUES('Комедия');
-
-INSERT INTO PUBLIC.GENRES
-(name)
-VALUES('Драма');
-
-INSERT INTO PUBLIC.GENRES
-(name)
-VALUES('Мультфильм');
-
-INSERT INTO PUBLIC.GENRES
-(name)
-VALUES('Триллер');
-
-INSERT INTO PUBLIC.GENRES
-(name)
-VALUES('Документальный');
-
-INSERT INTO PUBLIC.GENRES
-(name)
-VALUES('Боевик');
+INSERT INTO mpa (mpa_name)
+VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
